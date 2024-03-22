07ba2af4665f45c6e40dafb19c3332f9bb14e4ab
Praise Izuagie <izuagiepraise@gmail.com>
2024-03-22 12:13:28 +0100

    Add auto-create config

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   const connection = await mongoose.connect(env.MONGO_URI, {
     autoIndex: env.AUTO_INDEX,
+    autoCreate: env.AUTO_CREATE,
   });
   console.log("Connected to the DB");
   return connection;
