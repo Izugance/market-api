2de49fe3d2609fa7ad1b563bbb1e211630803e68
Praise Izuagie <izuagiepraise@gmail.com>
2024-03-22 12:14:55 +0100

    Move review route to review router

--- routes/product.js
+++ routes/product.js
@@ -6,19 +6,19 @@ import {
   getProduct,
   updateProduct,
   deleteProduct,
-  createReview,
 } from "../controllers/product.js";
-import { adminAuthMiddleware, userAuthMiddleware } from "../middleware/auth.js";
+import { adminAuthMiddleware } from "../middleware/auth.js";
 
 const productRouter = express.Router();
 
-productRouter.route("/").get(getAllProducts);
-productRouter.route("/create").post(adminAuthMiddleware, createProduct);
-productRouter.route("/:id").get(getProduct);
-productRouter.route("/:id/").patch(adminAuthMiddleware, updateProduct);
-productRouter.route("/:id").delete(adminAuthMiddleware, deleteProduct);
 productRouter
-  .route("/:id/reviews/create")
-  .post(userAuthMiddleware, createReview);
+  .route("/")
+  .get(getAllProducts)
+  .post(adminAuthMiddleware, createProduct);
+productRouter
+  .route("/:id")
+  .get(getProduct)
+  .patch(adminAuthMiddleware, updateProduct)
+  .delete(adminAuthMiddleware, deleteProduct);
 
 export { productRouter };
--- routes/review.js
+++ routes/review.js
@@ -1,13 +1,25 @@
 import express from "express";
 
-import { getReview, deleteReview } from "../controllers/review.js";
+import {
+  getReview,
+  getProductReviews,
+  deleteReview,
+  createReview,
+} from "../controllers/review.js";
 import { userAuthMiddleware } from "../middleware/auth.js";
 
 // Since a review is tied to a product upon its creation, we have the
 // createReview route in `./product.js`.
 const reviewRouter = express.Router();
 
-reviewRouter.route("/:id").get(getReview);
-reviewRouter.route("/:id").delete(userAuthMiddleware, deleteReview);
+reviewRouter
+  .route("/:id")
+  .get(getReview)
+  .route("/:id")
+  .delete(userAuthMiddleware, deleteReview);
+reviewRouter
+  .route("/:productId")
+  .get(getProductReviews)
+  .create(userAuthMiddleware, createReview);
 
 export { reviewRouter };
