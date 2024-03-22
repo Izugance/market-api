520803a3d63e5de109699c4ad6e3eb3f5de1780f
Praise Izuagie <izuagiepraise@gmail.com>
2024-03-22 12:40:29 +0100

    Create order routes

--- routes/order.js
+++ routes/order.js
@@ -1,5 +1,12 @@
 import express from "express";
 
+import { createOrder, cancelOrder, settleOrder } from "../controllers/order.js";
+import { userAuthMiddleware, adminAuthMiddleware } from "../middleware/auth.js";
+
 const orderRouter = express.Router();
+
+orderRouter.route("/").post(userAuthMiddleware, createOrder);
+orderRouter.route("/:id/cancel").put(adminAuthMiddleware, cancelOrder);
+orderRouter.route("/:id/settle").put(adminAuthMiddleware, settleOrder);
 
 export { orderRouter };
