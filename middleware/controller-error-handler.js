93279593e2296941aad8cb0606ee2f957d12d9f8
Praise Izuagie <izuagiepraise@gmail.com>
2024-03-25 21:19:46 +0100

    Implement error-handling logic

--- middleware/controller-error-handler.js
+++ middleware/controller-error-handler.js
@@ -1,9 +1,32 @@
 import { StatusCodes } from "http-status-codes";
 
-const controllerErrorHandler = async (req, res, next) => {
-  res
-    .status(StatusCodes.INTERNAL_SERVER_ERROR)
-    .json({ msg: "Server in progress" });
+const controllerErrorHandler = (err, req, res, next) => {
+  const error = {
+    msg:
+      err.msg || "Something went wrong. Please check parameters and try again",
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+  };
+
+  if (err.name === "ValidationError") {
+    let errorString = Object.values(err.errors)
+      .map((error) => error.message)
+      .join(", ");
+    error.msg = errorString;
+    error.statusCode = StatusCodes.BAD_REQUEST;
+  } else if (err.name === "CastError") {
+    error.msg =
+      `Could not find the requested resource with id ${err.value}. ` +
+      "Please verify the id and try again";
+    error.statusCode = StatusCodes.NOT_FOUND;
+  } else if (err.code === 11000) {
+    // Duplicate object creation error.
+    error.msg =
+      `Attempt at creating duplicate objects with unique ` +
+      `field(s) '${Object.keys(err.keyValue)}'`;
+    error.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  return res.status(error.statusCode).json({ msg: error.msg });
 };
 
 export { controllerErrorHandler };
--- middleware/endpoint-404-handler.js
+++ middleware/endpoint-404-handler.js
@@ -2,10 +2,10 @@ import { StatusCodes } from "http-status-codes";
 
 import { ResourceNotFoundError } from "../errors/index.js";
 
-const endpoint404Handler = async (req, res, next) => {
-  throw new ResourceNotFoundError(
-    `Requested endpoint ${req.url} doesn't exist with method ${req.method}.`
-  );
+const endpoint404Handler = (err, req, res, next) => {
+  return res.status(StatusCodes.NOT_FOUND).json({
+    msg: `Requested endpoint ${req.url} doesn't exist with method ${req.method}.`,
+  });
 };
 
 export { endpoint404Handler };
