4ecb6f81ede8e5f70957c4fcead0d894d424e2f3
Praise Izuagie <izuagiepraise@gmail.com>
2024-03-25 20:59:43 +0100

    Fix route methods

--- routes/review.js
+++ routes/review.js
@@ -8,18 +8,15 @@ import {
 } from "../controllers/review.js";
 import { userAuthMiddleware } from "../middleware/auth.js";
 
-// Since a review is tied to a product upon its creation, we have the
-// createReview route in `./product.js`.
 const reviewRouter = express.Router();
 
 reviewRouter
   .route("/:id")
   .get(getReview)
-  .route("/:id")
   .delete(userAuthMiddleware, deleteReview);
 reviewRouter
   .route("/:productId")
   .get(getProductReviews)
-  .create(userAuthMiddleware, createReview);
+  .post(userAuthMiddleware, createReview);
 
 export { reviewRouter };
