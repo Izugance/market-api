1c56090cc510a9531104fca8abdde7421cfca614
Praise Izuagie <izuagiepraise@gmail.com>
2024-03-27 15:20:08 +0100

    Add cart logic

--- controllers/cart.js
+++ controllers/cart.js
@@ -4,70 +4,53 @@ import { StatusCodes } from "http-status-codes";
 import { Cart } from "../models/Cart.js";
 import { ResourceNotFoundError } from "../errors/resource.js";
 
-// Adding cart items is a progressive endeavor.
+// Adding cart items is a progressive endeavor. User carts are auto-
+// created upon signup.
 
 /** GET. */
+// DO WE POPULATE THE 'PRODUCT' PATH?
 const getUserCart = asyncHandler(async (req, res) => {
-  // Will always get unique Cart-User pair.
-  const cart = await Cart.findOne({ user: req.user.userId }).exec();
+  const cart = await Cart.find({ user: req.user.userId })
+    .select("items")
+    .exec();
   res.status(StatusCodes.OK).json({ cart });
 });
 
 // Add to cart.
 /** UPDATE. */
 const createCartItem = asyncHandler(async (req, res) => {
-  const { productId, quantity } = req.body;
-  await Cart.updateOne(
+  await Cart.findOneAndUpdate(
     { user: req.user.userId },
-    {
-      $push: {
-        items: {
-          product: productId,
-          quantity,
-        },
-      },
-    }
+    { $push: { items: req.body } },
+    { runValidators: true }
   ).exec();
-  res
-    .status(StatusCodes.CREATED)
-    .json({ cartItem: { product: productId, quantity } });
+  res.status(StatusCodes.CREATED);
 });
 
-// When the client gets the user's cart, we return implicit cartItem
-// ids (recall subdocument array). Hence, there's an opportunity to
-// pass the implicit cartItem's id to this endpoint.
-/** UPDATE. */
-const updateCartItemQuantity = asyncHandler(async (req, res) => {
-  // const quantity = Number(req.body.quantity);
+const updateCartItem = asyncHandler(async (req, res) => {
+  // Updatable field is quantity.
+  // IS THERE A WAY TO REDUCE THIS TO ONE QUERY?
   const cart = await Cart.findOne({ user: req.user.userId }).exec();
   const item = cart.items.id(req.params.itemId);
-  if (!item) {
-    throw new ResourceNotFoundError(
-      `Cart item '${req.params.itemId}' does not exist for user ${req.user.userId}`
-    );
-  }
-  item.quantity = req.body.quantity; // save() -> validation.
+  item.qty = Number(req.body.quantity);
   await cart.save();
   res.status(StatusCodes.NO_CONTENT);
 });
 
-// Though an update, logically a delete.
+// LOGICALLY A DELETE, PRACTICALLY AN UPDATE.
 /** DELETE. */
 const deleteCartItem = asyncHandler(async (req, res) => {
-  await Cart.updateOne(
-    {
-      user: req.user.userId,
-    },
-    { $pull: { items: { product: req.params.productId } } }
+  await Cart.findOneAndUpdate(
+    { user: req.user.userId },
+    { $pull: { "items._id": req.params.itemId } }
   ).exec();
-  // We assume client code updates displayed cart items.
   res.status(StatusCodes.NO_CONTENT);
 });
 
-// Should be called after order creation verification.
+// LOGICALLY A DELETE, PRACTICALLY AN UPDATE.
 /** DELETE. */
 const clearUserCart = asyncHandler(async (req, res) => {
-  await Cart.updateOne(
+  await Cart.findOneAndUpdate(
     { user: req.user.userId },
     { $set: { items: [] } }
   ).exec();
@@ -78,6 +61,6 @@ export {
   getUserCart,
   createCartItem,
   deleteCartItem,
-  updateCartItemQuantity,
+  updateCartItem,
   clearUserCart,
 };
