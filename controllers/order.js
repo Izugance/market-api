c6a219c4bb1098ca5e9b8ffce565f19e50bc494b
Praise Izuagie <izuagiepraise@gmail.com>
2024-03-27 15:14:12 +0100

    Refactor order logic

--- controllers/order.js
+++ controllers/order.js
@@ -1,23 +1,82 @@
 import asyncHandler from "express-async-handler";
 
+import { connectDB } from "../config/db.js";
 import { Order } from "../models/Order.js";
 import { Cart } from "../models/Cart.js";
 import { StatusCodes } from "http-status-codes";
-
-// Payment options: Stripe, paypal, mastercard.
-
-// We assume the following checkout flow: The user provides contact
-// info (phone, email), shipping info (country, addy, postal code,
-// city, province). Then payment, then review and place order.
+import { BadRequestError, ResourceNotFoundError } from "../errors/index.js";
 
 // Orders are built from a user's cart.
+
+// COUPLING ORDER CREATION WITH CART CLEARING. MAKES SENSE???
+/** POST. */
 const createOrder = asyncHandler(async (req, res) => {
-  const cart = await Cart.findOne({ userId: req.user.userId });
-  const order = await Order.create({
-    userId: req.user.userId,
-    items: cart.items,
-  }).exec();
-  res.status(StatusCodes.CREATED).json({ order });
+  const connection = await connectDB();
+  const session = await connection.startSession();
+
+  session.withTransaction(async () => {
+    // We return the cart's previous state (only its items). These
+    // items have the same shape as the order items.
+    let items = await Cart.findOneAndUpdate(
+      { userId: req.user.userId },
+      { $set: { items: [] } },
+      { new: false }
+    )
+      .select("items")
+      .exec();
+
+    if (!items) {
+      throw new ResourceNotFoundError(
+        `No cart items to build order for user ${req.user.userId}`
+      );
+    }
+
+    items = items.toArray();
+
+    await Order.create({
+      userId: req.user.userId,
+      items: items,
+    });
+    res.status(StatusCodes.CREATED);
+  });
+
+  await session.endSession();
+  await connection.close();
 });
 
-export { createOrder, checkoutOrder };
+/** GET. */
+const getUserOrders = asyncHandler(async () => {
+  const orders = await Order.find({ user: req.user.userId });
+  res.status(StatusCodes.OK).json(orders);
+});
+
+/** GET. */
+const getOrdersAdmin = asyncHandler(async () => {
+  // PROVIDE QUERY ADN OTHER PARAMS LATER.
+  const orders = await Order.find(req.query);
+  res.status(StatusCodes.OK).json(orders);
+});
+
+/** UPDATE. */
+const cancelOrder = asyncHandler(async () => {
+  const order = await Order.cancel(req.params.id, req.user.userId);
+  if (!order) {
+    throw new ResourceNotFoundError(
+      `Order ${req.params.id} does not exist for user ${req.user.userId}`
+    );
+  }
+  res.status(StatusCodes.NO_CONTENT);
+});
+
+/** UPDATE. */
+const settleOrder = asyncHandler(async (req, res) => {
+  const order = await Order.settle(req.params.id, req.user.userId);
+  if (!order) {
+    throw new ResourceNotFoundError(
+      `Order ${req.params.id} does not exist for user ${req.user.userId}`
+    );
+  }
+  res.status(StatusCodes.NO_CONTENT);
+});
+
+export { createOrder, cancelOrder, settleOrder };
--- routes/order.js
+++ routes/order.js
@@ -5,7 +5,11 @@ import { userAuthMiddleware, adminAuthMiddleware } from "../middleware/auth.js";
 
 const orderRouter = express.Router();
 
-orderRouter.route("/").post(userAuthMiddleware, createOrder);
+orderRouter
+  .route("/")
+  .post(userAuthMiddleware, createOrder)
+  .get(userAuthMiddleware, getUserOrders);
+orderRouter.route("/admin").get(adminAuthMiddleware, getOrdersAdmin);
 orderRouter.route("/:id/cancel").patch(adminAuthMiddleware, cancelOrder);
 orderRouter.route("/:id/settle").patch(adminAuthMiddleware, settleOrder);
 
